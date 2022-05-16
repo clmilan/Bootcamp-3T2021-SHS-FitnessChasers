176e0802894c143aa23e9ff8665e77a541f325d1
clmilan <clmilan@student.apc.edu.ph>
2022-05-16 15:17:34 +0800

    hello

--- trial.py
+++ trial.py
@@ -2,6 +2,16 @@
 
 # Import and initialize the pygame library
 import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+pygame.display.set_caption("FitnessChasers: Catch the Healthy Food")
 pygame.init()
 
 # Set up the drawing window
@@ -24,6 +34,17 @@ while running:
 
     # Flip the display
     pygame.display.flip()
-
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False       
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
 # Done! Time to quit.
 pygame.quit()
