c6a3fd26813d00d4d8fb5ec40cb9e70679d1955b
mtdeguzman <mtdeguzman@student.apc.edu.ph>
2022-05-17 09:45:15 +0800

    remove other comments

--- master.py
+++ master.py
@@ -25,17 +25,6 @@ icon = pygame.image.load('background.png')
 pygame.display.set_icon(icon)
 
 
-
-# commented codes are for follow-up user interface improvements
-# aesthetics 
-# white color 
-# color = (255,255,255) 
-  
-# light shade of the button 
-# color_light = (170,170,170) 
-  
-# dark shade of the button 
-# color_dark = (100,100,100) 
   
 # stores the width of the 
 # screen into a variable 
@@ -103,21 +92,6 @@ def main_menu ():
             if ev.button == 1: 
                 click = True
         
-        # screen.fill((60,25,60)) 
-      # stores the (x,y) coordinates into 
-      # the variable as a tuple 
-     #  mouse = pygame.mouse.get_pos() 
-      
-      # if mouse is hovered on a button it 
-      # changes to lighter shade 
-     # if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
-      #  pygame.draw.rect(screen,color_light,[width/2,height/2,140,40]) 
-          
-      #else: 
-       # pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40]) 
-      
-       # superimposing the text onto our button 
-      # screen.blit(text , (width/2+50,height/2)) 
       
       # updates the frames of the game 
       pygame.display.update() 
